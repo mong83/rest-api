6031fc08dc5e9199e5aaa5129fd094fe16028ac9
mong83 <06007sk@gmail.com>
2020-03-28 07:16:37 +0900

    비즈니스로직적용(event , eventcontroller update함수구현)

--- src/main/java/com/study/event/Event.java
+++ src/main/java/com/study/event/Event.java
@@ -30,4 +30,18 @@ public class Event {
         private EventStatus eventStatus;
 
 
+        public void update() {
+                if(this.basePrice == 0 && this.maxPrice == 0 ){
+                        this.free =true ;
+                }else {
+                        this.free = false;
+                }
+
+                if(this.location == null || this.location.isEmpty()){
+                        this.offline =false;
+
+                }else {
+                        this.offline = true;
+                }
+        }
 }
