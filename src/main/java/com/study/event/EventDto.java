e6b409e4a34824673ae4b3e102b1e519b7dd716c
mong83 <06007sk@gmail.com>
2020-03-27 13:32:19 +0900

    Bad Request 처리하기

--- src/main/java/com/study/event/EventDto.java
+++ src/main/java/com/study/event/EventDto.java
@@ -2,6 +2,8 @@ package com.study.event;
 
 import lombok.*;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
 @Builder
@@ -10,15 +12,25 @@ import java.time.LocalDateTime;
 @Data
 public class EventDto {
 
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String description;
+    @NonNull
     private LocalDateTime beginEnrollmentDateTime;
+    @NonNull
     private LocalDateTime closeEnrollmentDateTime;
+    @NonNull
     private LocalDateTime beginEventDateTime;
+    @NonNull
     private LocalDateTime endEventDateTime;
+
     private String location;
+    @Min(0)
     private int basePrice;
+    @Min(0)
     private int maxPrice;
+    @Min(0)
     private int limitOfEnrollment;
 
 }
--- src/test/java/com/study/event/EventControllerTests.java
+++ src/test/java/com/study/event/EventControllerTests.java
@@ -67,6 +67,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력받을수 없는 값을 사용한경우에 에러발생한케이스")
     public void createEvent_Bad_Request() throws Exception {
 
         Event event = Event.builder()
@@ -95,5 +96,42 @@ public class EventControllerTests {
 
         ;
     }
-}
+
+    @Test
+    @TestDescription("입력값이 비어있는경우에 에러발생한케이스")
+    public void createEvent_Bad_Request_Empty_Input() throws Exception {
+
+            EventDto eventDto  = EventDto.builder().build();
+
+            this.mockMvc.perform(post("/api/events/")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(this.objectMapper.writeValueAsString(eventDto)))
+                    .andExpect(status().isBadRequest());
+
+    }
+
+    @Test
+    @TestDescription("입력값이 잘못된 경우에 에러발생한케이스")
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+
+        EventDto eventDto  = EventDto.builder()
+                .name("spring")
+                .description("rest api")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,23,14,21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,24,14,21))
+                .beginEventDateTime(LocalDateTime.of(2018,11,27,14,21))
+                .endEventDateTime(LocalDateTime.of(2018,11,26,14,21))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
+
+        this.mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
+
+    }
+    }
 
