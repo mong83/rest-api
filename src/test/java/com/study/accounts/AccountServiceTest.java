f800b1e8e969ee72d368d05f6c67f52a9b18fe5b
mong83 <06007sk@gmail.com>
2020-04-01 21:26:32 +0900

    예외처리 방식 3가지

--- src/test/java/com/study/accounts/AccountServiceTest.java
+++ src/test/java/com/study/accounts/AccountServiceTest.java
@@ -1,11 +1,15 @@
 package com.study.accounts;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,12 +18,16 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -51,5 +59,27 @@ public class AccountServiceTest {
         assertThat(userDetails.getPassword()).isEqualTo(password);
     }
 
+    @Test
+     public void findByUsernameFail() {
+        String username = "random@email.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
 
+        accountService.loadUserByUsername(username);
+
+
+
+    /*
+     @Test(expected = UsernameNotFoundException.class)
+      public void findByUsernameFail() {
+        String username = "random@email.com";
+        try{
+            accountService.loadUserByUsername(username);
+            fail("supposed to be failed");
+        }catch (UsernameNotFoundException e){
+            assertThat(e.getMessage()).containsSequence(username);
+        }
+
+    */
+    }
 }
